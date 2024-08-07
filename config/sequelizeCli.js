b137f44eaecc247807a31a042429a316f7d8f5aa
Carlos Henrique <carloshenriquepcrc@gmail.com>
2024-08-07 18:50:32 -0300

    configuração das variáveis de ambiente

--- config/sequelizeCli.js
+++ config/sequelizeCli.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
     development: {
-        dialect: 'postgres',
-        host: 'localhost',
-        port: '5432',
-        database: 'langflix_development',
-        username: 'langflix',
-        password: '9102'
+        dialect: process.env.DB_DIALECT,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD
     }
-}
+};
